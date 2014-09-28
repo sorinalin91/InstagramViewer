ce0d216a769fbf1da58efb36bcf4f8ec47d96955
Pavel Sorin-Alin <sorin.alin91@yahoo.com>
2014-09-28 04:26:48 +0300

    Update
    -Push photos to webapte;
    -Push comments to webapge;
    -Get more photos when you click on more;

--- js/controllers.js
+++ js/controllers.js
@@ -16,7 +16,7 @@ angular.module("myApp", [])
             return {
                 fetchPopular: function(callback) {
 
-                    var endPoint = "https://api.instagram.com/v1/media/popular?client_id=642176ece1e7445e99244cec26f4de1f&callback=JSON_CALLBACK";
+                    var endPoint = "https://api.instagram.com/v1/media/popular?client_id=b1af7fc1ba77416787170b046f97a192&callback=JSON_CALLBACK";
 
                     $http.jsonp(endPoint).success(function(response) {
                         callback(response.data);
@@ -41,8 +41,6 @@ angular.module("myApp", [])
       };
       $scope.getMore();
       
-        $scope.tags = [
-            'Bootstrap', 'AngularJS', 'Instagram', 'Factory'
-        ]
+        
     });
 // you may add more controllers below
